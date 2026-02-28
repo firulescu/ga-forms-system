023f13eaa71ae5e3b7ed0036b12813ca814a945a
Nony <firulescum@yahoo.com>
2026-02-28 23:01:01 +0000

    fix: hardcode Firebase URL so operator phones don't need localStorage

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,6 +3,6 @@
 // ============================================================
 const FIREBASE_URL = (() => {
   try {
-    return localStorage.getItem('ga_firebase_url') || 'https://ga-forms-rql-default-rtdb.europe-west1.firebasedatabase.app'\;
-  } catch { return 'https://ga-forms-rql-default-rtdb.europe-west1.firebasedatabase.app'\; }
+    return localStorage.getItem('ga_firebase_url') || 'https://ga-forms-rql-default-rtdb.europe-west1.firebasedatabase.app';
+  } catch { return 'https://ga-forms-rql-default-rtdb.europe-west1.firebasedatabase.app'; }
 })();
